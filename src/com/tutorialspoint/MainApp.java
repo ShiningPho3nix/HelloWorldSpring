d503d55b76a1597606e055d34ca6b364e50a17ff
ShiningPho3nix <son-goku__styler@online.de>
2019-01-14 19:00:13 +0100

    Neue Klassen für Dependency Injection, diverse Umbenennungen, Logging.
    Beans.xml -> ApplicationContext.xml
    Address und Employee sind neue Klassen.
    Logging mit log4j2, noch nicht vollständig einsatzbereit.

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -3,14 +3,31 @@ package com.tutorialspoint;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class MainApp {
 
-	public static void main(String[] args) {
+	private static Logger log = LogManager.getLogger(com.tutorialspoint.MainApp.class.getName());
 
-		@SuppressWarnings("resource")
-		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-		obj.getMessage();
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		log.trace("Trace message. Also: Entering application.");
+		log.debug("Debug message");
+		log.info("Info message");
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+		log.warn("Warn message. Also: Going to create HelloWord Obj and Employee");
+
+		HelloWorld hello = (HelloWorld) context.getBean("helloWorld");
+		Employee employee = (Employee) context.getBean("employee");
+
+		hello.getMessage();
+		employee.printEmployee();
+
+		log.error("Error message");
+		log.fatal("Fatal message, also: Exiting the program");
 	}
 
 }
